281efa082849cb77d3e6d8c2f01067d4c054102a
JUN-KAI-FAN <kenny45452@gmail.com>
2021-08-15 12:17:36 +0800

    Update test66.py

--- test66.py
+++ test66.py
@@ -3,6 +3,7 @@ import requests
 import bs4
 import datetime
 import time
+import json
 
 
 
@@ -34,6 +35,13 @@ def get_one_page(URL):
     # 因為我們有多個Tags存放在 List titles中。
     # 所以需要使用for 迴圈將逐筆將List 
     for t in titles:
+
+        with open('setting.json',mode='r',encoding='utf8') as jfile:
+            jdata=json.load(jfile)
+        jdata['URL'] = "https://www.ptt.cc"+t.a.get("href")
+        #time.sleep(0.1)
+        with open('setting.json',mode='w',encoding='utf8') as jfile:
+            jdata=json.dump(jdata, jfile, indent = 4)
         print(t.text.strip())  #strip 是把空白去掉的意思。
         print("https://www.ptt.cc"+t.a.get("href"))
         
