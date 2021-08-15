caeb79dc266e19fc697b2c7f764fd668e588fe39
JUN-KAI-FAN <88937319+JUN-KAI-FAN@users.noreply.github.com>
2021-08-15 10:32:37 +0800

    Add files via upload

---
+++ test66.py
@@ -0,0 +1,12 @@
+import requests 
+# 導入 BeautifulSoup 模組(module)：解析HTML 語法工具
+import bs4
+import datetime
+import time
+
+
+now_time = datetime.datetime.now().strftime('%H:%M')
+
+if(now_time == '17:34'):
+    print(now_time)
+
